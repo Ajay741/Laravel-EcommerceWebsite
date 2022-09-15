1d642860ed228a3bbe1b39c4f595276139c573ff
Ajay <ajaysanjaysingh12345@gmail.com>
2022-09-15 15:30:10 +0530

    Initial commit

---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    
+    @include('admin.css')
+
+  </head>
+  <body>
+    
+    <!-- partial -->
+      
+    @include('admin.sidebar')
+
+    @include('admin.navbar')
+
+    @include('admin.body')
+
+    <!-- partial -->
+
+    
+    @include('admin.script') 
+    
+    
+    <!-- partial -->
+        
+  </body>
+</html>
